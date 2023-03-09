c6d2f17424c4f1053ceb01b5c440014742c2d4ef
khalid <dev.khalidhossain@gmail.com>
2023-03-09 20:58:05 +0600

    decrease count logic change.

--- react-hooks/.history/src/Components/Hooks/UseState_20230309205603.jsx
+++ react-hooks/.history/src/Components/Hooks/UseState_20230309205603.jsx
@@ -7,9 +7,9 @@ const UseState = () => {
     
     return (
         <div className='useStateHook'>
-           <button onClick={()=>{setCount(count+1); console.log('run')}}> + </button>
+           <button onClick={()=>{setCount(count+2); console.log('run')}}> + </button>
               <span>{count}</span>
-           <button onClick={()=>{setCount(count<=0?0:count-1); console.log('run')}}> - </button>
+           <button onClick={()=>{setCount(count<=0?0:count-2); console.log('run')}}> - </button>
         </div>
     );
 }
