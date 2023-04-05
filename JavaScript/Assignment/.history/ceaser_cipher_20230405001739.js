8aa8e62e2632a1e0cae6e25e1e1789f08f445934
khalid <dev.khalidhossain@gmail.com>
2023-04-05 23:58:25 +0600

    React Hooks practice

---
+++ JavaScript/Assignment/.history/ceaser_cipher_20230405001739.js
@@ -0,0 +1,28 @@
+const caesarEncrypt = (plainText, shift) => {
+let cipherText = "";
+for (let i = 0; i < plainText.length; i++) {
+const charCode = plainText.charCodeAt(i);
+if (charCode >= 65 && charCode <= 90) { // Upper case letters
+cipherText += String.fromCharCode((charCode - 65 + shift) % 26 + 65);
+} else if (charCode >= 97 && charCode <= 122) { // Lower case letters
+cipherText += String.fromCharCode((charCode - 97 + shift) % 26 + 97);
+} else { // Non-alphabetic characters
+cipherText += plainText.charAt(i);
+}
+}
+
+ return cipherText;
+};
+const caesarDecrypt = (cipherText, shift) => {
+let plainText = "";
+for (let i = 0; i < cipherText.length; i++) {
+const charCode = cipherText.charCodeAt(i);
+if (charCode >= 65 && charCode <= 90) { // Upper case letters
+plainText += String.fromCharCode((charCode - 65 - shift + 26) % 26 + 65);
+} else if (charCode >= 97 && charCode <= 122) { // Lower case letters
+plainText += String.fromCharCode((charCode - 97 - shift + 26) % 26 + 97);
+} else { // Non-alphabetic characters
+plainText += cipherText.charAt(i);
+}
+}
+retur
